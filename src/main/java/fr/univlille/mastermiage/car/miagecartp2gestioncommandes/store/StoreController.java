7b110f10e1deed8e5a0bd81080f4a0bdb664d5b5
AymericJak <aymericjakpro@gmail.com>
2025-02-06 12:08:22 +0100

    Implement store initialisation method

--- src/main/java/fr/univlille/mastermiage/car/miagecartp2gestioncommandes/store/StoreController.java
+++ src/main/java/fr/univlille/mastermiage/car/miagecartp2gestioncommandes/store/StoreController.java
@@ -1,17 +1,32 @@
 package fr.univlille.mastermiage.car.miagecartp2gestioncommandes.store;
 
+import fr.univlille.mastermiage.car.miagecartp2gestioncommandes.client.IClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
 @RequestMapping("/store")
 public class StoreController {
 
+    @Autowired
+    private IClient clientService;
+
     @GetMapping("/home")
     public String home() {
         return "store/home";
     }
+
+    @GetMapping("/init")
+    @ResponseBody
+    public RedirectView init() {
+        clientService.init();
+        return new RedirectView("/store/home");
+    }
+
 
 }
