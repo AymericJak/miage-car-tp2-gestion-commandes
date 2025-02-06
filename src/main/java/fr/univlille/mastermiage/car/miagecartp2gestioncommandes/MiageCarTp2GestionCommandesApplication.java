a1e4a91761760d7e618520ad2ee9a9fc14abe404
AymericJak <aymericjakpro@gmail.com>
2025-02-06 09:26:20 +0100

    first commit

---
+++ src/main/java/fr/univlille/mastermiage/car/miagecartp2gestioncommandes/MiageCarTp2GestionCommandesApplication.java
@@ -0,0 +1,13 @@
+package fr.univlille.mastermiage.car.miagecartp2gestioncommandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageCarTp2GestionCommandesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiageCarTp2GestionCommandesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/univlille/mastermiage/car/miagecartp2gestioncommandes/MiageCarTp2GestionCommandesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univlille.mastermiage.car.miagecartp2gestioncommandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiageCarTp2GestionCommandesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
